1b774bbfdac6d74cfc879c538eca37759b724f87
CherylLim <85498185+dracolim@users.noreply.github.com>
2023-01-01 16:57:18 +0800

    user can now search for nearby food places in their area

--- chatbot2.py
+++ chatbot2.py
@@ -26,7 +26,7 @@ def predict():
     lat = request.json['lat']
     lon = request.json['lon']
     ints = predict_class(message)
-    res = get_response(ints, intents,lat, lon)
+    res = get_response(message,ints, intents,lat, lon)
     return jsonify({'response': res, 'lat': lat, 'lon': lon})
 
 lemmatizer = WordNetLemmatizer()
@@ -59,13 +59,22 @@ def predict_class(sentence):
     return_list = []
     for r in results:
         return_list.append({"intent" : classes[r[0]], "probability": str(r[1])})
-
     return return_list
 
-def get_response(intents_list, intents_json, lat , lon):
+def get_response(message, intents_list, intents_json, lat , lon):
     tag = intents_list[0]['intent']
     if tag == "weather":
         result = get_weather(lat, lon)
+    elif tag == "nearby restaurants":
+        radius = ''
+        last_index = 0
+        for i in range(len(message)):
+            if message[i].isdigit():
+                radius += message[i]
+                last_index = i
+        if message[last_index+1] == 'k':
+            radius = int(radius) * 1000
+        result = get_nearby_restaurants(lat, lon,radius)
     else: 
         list_of_intents = intents_json['intents']
         for i in list_of_intents:
@@ -75,7 +84,6 @@ def get_response(intents_list, intents_json, lat , lon):
     return result
 
 def get_weather(lat, lon):
-    # Replace YOUR_API_KEY with your actual API key
     with open('api_key.txt', 'r') as f:
         api_keys = []
         for line in f:
@@ -88,19 +96,42 @@ def get_weather(lat, lon):
     api_key = api_keys[0]
     # Send a request to the OpenWeatherMap API to get the current weather for the city
     response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}")
+    if response.status_code == 200:
+        data = response.json()
+        temperature = round(int(data["main"]["temp"]) - 273.15 , 2)
+        description = data["weather"][0]["description"]
+        humidity = data["main"]["humidity"]
+        return f"The current temperature is {str(temperature)}°C and the sky is {description} with humidity of {humidity}"
+    else:
+        return "Sorry, there was an error getting the weather data."
+
+def get_nearby_restaurants(lat, lon, radius):
+    # Replace YOUR_API_KEY with your actual API key
+    with open('api_key.txt', 'r') as f:
+        api_keys = []
+        for line in f:
+            list = line.strip().split('=')
+            if(len(list) == 2):
+                value = list[1]
+                api_keys.append(value)
+            else:
+                break
+    api_key = api_keys[1]
+    # Construct the URL for the request
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat},{lon}&radius={radius}&type=restaurant&key={api_key}"
+    # Send the request to the Google Places API
+    response = requests.get(url)
     # Check the status code of the response to ensure that the request was successful
     if response.status_code == 200:
         # If the request was successful, parse the JSON data from the response
         data = response.json()
-        # Get the temperature and weather description from the data
-        temperature = round(int(data["main"]["temp"]) - 273.15 , 2)
-        description = data["weather"][0]["description"]
-        humidity = data["main"]["humidity"]
-        # Return the temperature and description as a string
-        return f"The current temperature is {str(temperature)}°C and the sky is {description} with humidity of {humidity}"
+        # Get the list of restaurants from the data
+        restaurants = data["results"]
+        # Return the list of restaurants
+        return restaurants
     else:
-        # If the request was not successful, return an error message
-        return "Sorry, there was an error getting the weather data."
+        # If the request was not successful, return an empty list
+        return []
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5008, debug=True)
